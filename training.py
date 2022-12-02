c442e0cab790f1f903e3af5c4fb85adabc8ed113
Shingome <mishakkoles@gmai.com>
2022-12-02 21:16:15 +0300

    new model

--- training.py
+++ training.py
@@ -35,6 +35,14 @@ if __name__ == "__main__":
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(64, (2, 2), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(256, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(512, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(1024, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.5))
@@ -45,18 +53,18 @@ if __name__ == "__main__":
     history = model.fit(x=x_train,
                         y=y_train,
                         validation_data=(x_val, y_val),
-                        epochs=5,
+                        epochs=20,
                         shuffle=True,
                         batch_size=16)
 
     plot_model(model,
-               to_file="models/plot_ep5_2.png",
+               to_file="models/plot_ep20_4.png",
                show_dtype=True,
                show_shapes=True,
                show_layer_names=True,
                show_layer_activations=True)
 
-    model.save("models/train_ep5_2.h5", save_format="h5")
+    model.save("models/train_ep20_4.h5", save_format="h5")
 
     pd.DataFrame(history.history).plot(figsize=(8, 5))
 
